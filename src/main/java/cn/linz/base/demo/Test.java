0407c70f78d9220dea15fa12fa94bb8269b8cecd
taoguoliang <guoliang.tao@ideamake.cn>
2022-01-11 11:21:55 +0800

    fix(demo): key

--- src/main/java/cn/linz/base/demo/Test.java
+++ src/main/java/cn/linz/base/demo/Test.java
@@ -12,7 +12,7 @@ import cn.linz.base.common.model.BaseEntity;
 import javax.persistence.Entity;
 
 @Entity
-public class Test extends BaseEntity<Integer> {
+public class Test extends BaseEntity<String> {
 
     private static final long serialVersionUID = -102773664852850638L;
 
